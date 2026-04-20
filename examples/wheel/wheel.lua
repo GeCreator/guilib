4c7dd15dbf1531247700d73627072d4641c741d2
gecreator <ge-rom@mail.ru>
2026-04-20 17:12:45 +0400

    update wheel example

--- examples/wheel/wheel.lua
+++ examples/wheel/wheel.lua
@@ -1,5 +1,4 @@
-local FACTOR = 0.04
- local function zoom(scale)
+local function zoom(scale)
   return function(e)
     local s = gui.get_scale(e.node)
     s.x = s.x + s.x*scale
@@ -8,19 +7,38 @@ local FACTOR = 0.04
   end
 end
 
+local function rotate(angle)
+  return function(e)
+    local r = gui.get_euler(e.node)
+    r.z = r.z + angle
+    gui.set_euler(e.node, r)
+  end
+end
+
+
+local function move(step)
+  return function(e)
+    local p = gui.get_position(e.node)
+    p.y = p.y + step
+    gui.set_position(e.node, p)
+  end
+end
+
+
 ---@param guilib GuiLib
 ---@param log fun(text: string)
 return function(guilib, _)
   guilib.add("wheel1", {
-    [hash('mouse_wheel_up')] = zoom(FACTOR),
-    [hash('mouse_wheel_down')] = zoom(-FACTOR),
+    [hash('mouse_wheel_up')] = zoom(0.04),
+    [hash('mouse_wheel_down')] = zoom(-0.04),
   })
   guilib.add("wheel2", {
-    [hash('mouse_wheel_up')] = zoom(FACTOR),
-    [hash('mouse_wheel_down')] = zoom(-FACTOR),
+    [hash('mouse_wheel_up')] = rotate(3),
+    [hash('mouse_wheel_down')] = rotate(-3),
   })
   guilib.add("wheel3", {
-    [hash('mouse_wheel_up')] = zoom(FACTOR),
-    [hash('mouse_wheel_down')] = zoom(-FACTOR),
+    [hash('mouse_wheel_up')] = move(1),
+    [hash('mouse_wheel_down')] = move(-1),
   })
+  -- guilib.add("wheel3", t)
 end
