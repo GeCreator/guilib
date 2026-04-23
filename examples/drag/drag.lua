02ae5a984f0ec9a8b14e21e4e72d093165c2e182
gecreator <ge-rom@mail.ru>
2026-04-23 21:25:36 +0400

    update annotations

--- examples/drag/drag.lua
+++ examples/drag/drag.lua
@@ -1,4 +1,5 @@
-local function create_box(guilib, name, log)
+---@param guilib GuiLib
+local function create_draggable_box(guilib, name, log)
   local offset = vmath.vector3(0)
   guilib.add(name, {
     drag = function(action)
@@ -9,7 +10,7 @@ local function create_box(guilib, name, log)
       elseif action.drag_end then
         log("Drag End")
       else
-        gui.set_screen_position(action.node, mouse_pos- offset)
+        gui.set_screen_position(action.node, mouse_pos - offset)
         log("Drag")
       end
     end,
@@ -18,7 +19,7 @@ end
 ---@param guilib GuiLib
 ---@param log fun(text: string)
 return function(guilib, log)
-  create_box(guilib, "box3", log)
-  create_box(guilib, "box2", log)
-  create_box(guilib, "box1", log)
+  create_draggable_box(guilib, "box3", log)
+  create_draggable_box(guilib, "box2", log)
+  create_draggable_box(guilib, "box1", log)
 end
