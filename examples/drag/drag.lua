381b6b683b737cbe9edba2bb7a76ddf159d8c13f
gecreator <ge-rom@mail.ru>
2026-04-28 16:27:17 +0400

    component drag

--- examples/drag/drag.lua
+++ examples/drag/drag.lua
@@ -1,26 +1,15 @@
+local components = require("guilib.components")
 ---@param guilib GuiLib
-local function create_draggable_box(guilib, name, log)
-  local offset = vmath.vector3(0)
-  guilib.add(name, {
-    pressed = function(action)
-      log("Drag Begin")
-      local mouse_pos = vmath.vector3(action.screen_x, action.screen_y, 0)
-      offset = mouse_pos - gui.get_screen_position(action.node)
-    end,
-    hold = function(action)
-      log("Drag")
-      local mouse_pos = vmath.vector3(action.screen_x, action.screen_y, 0)
-      gui.set_screen_position(action.node, mouse_pos - offset)
-    end,
-    released = function(action)
-      log("Drag End")
-    end
-    })
+local function create_box(element, log)
+  element.on("pressed",  function() log("Drag Begin") end)
+  element.on("hold",  function() log("Drag") end)
+  element.on("released",  function() log("Drag End") end)
+  return element
 end
 ---@param guilib GuiLib
 ---@param log fun(text: string)
 return function(guilib, log)
-  create_draggable_box(guilib, "box1", log)
-  create_draggable_box(guilib, "box2", log)
-  create_draggable_box(guilib, "box3", log)
+  components.draggable(create_box(guilib.add("box1"), log))
+  components.draggable(create_box(guilib.add("box2"), log))
+  components.draggable(create_box(guilib.add("box3"), log))
 end
---
+++ guilib/components.lua
@@ -0,0 +1,18 @@
+---@param element GuiLibElement
+---@return GuiLibElement
+local function draggable(element)
+  local offset = vmath.vector3(0)
+  element.on("pressed", function(action)
+    local mouse_pos = vmath.vector3(action.screen_x, action.screen_y, 0)
+    offset = mouse_pos - gui.get_screen_position(action.node)
+  end)
+  element.on("hold", function(action)
+    local mouse_pos = vmath.vector3(action.screen_x, action.screen_y, 0)
+    gui.set_screen_position(action.node, mouse_pos - offset)
+  end)
+  return element
+end
+
+return {
+  draggable = draggable
+}
