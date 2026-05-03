b4d6850398f247daef2279bf80b61ccdfda76939
gecreator <ge-rom@mail.ru>
2026-05-03 19:51:39 +0400

    move slider example to components exaple

---
+++ examples/components/slider/slider.lua
@@ -0,0 +1,7 @@
+local slider = require("components.slider.slider")
+
+---@param element GuiLibElement
+---@param log fun(text: string)
+return function(element, log)
+  local s = slider(element, "slider")
+end
